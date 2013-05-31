a03d54abb810f714e3a212db1cd2aa0c562c3f7b
Ka-ming Chan <ming4883@gmail.com>
2013-05-31 17:00:10 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exp3::Application.config.secret_token = '1542eaf8bfdf5969481826fccd1d82756e03326e4b3c0741f0168be388f14e893f477a79631237f0c4324500fc3b8fbe1be032e321bc0692e5b34baf39162510'
