ddca0f93f94615af9f22793f7be16a9305a63e44
Ka-ming Chan <ming4883@gmail.com>
2013-06-07 19:37:45 +0800

    added more textures and chain-rule method to demo_derivative_maps.html

--- public/exp3/assets/derivativemaps.fs
+++ public/exp3/assets/derivativemaps.fs
@@ -21,22 +21,49 @@ vec3 PerturbNormal( vec3 n, vec3 dpdx, vec3 dpdy, float dhdx, float dhdy )
     return normalize( n - CalculateSurfaceGradient( n, dpdx, dpdy, dhdx, dhdy ) );
 }
 
+float ApplyChainRule( float dhdu, float dhdv, float dud_, float dvd_ )
+{
+    return dhdu * dud_ + dhdv * dvd_;
+}
+
 void main()
 {
-    float bumpHeight = texture2D( heightMap, v_txc ).r * bumpness;
-    
-    vec3 wsNormal = normalize( v_nrm );
-    
     vec3 dpdx = dFdx( v_pos );
     vec3 dpdy = dFdy( v_pos );
  
+#if 0
+
+    float bumpHeight = texture2D( heightMap, v_txc ).b * bumpness;
+    
     float dhdx = dFdx( bumpHeight );
     float dhdy = dFdy( bumpHeight );
+
+#else
+    vec2 bumpGrad = ( texture2D( heightMap, v_txc ).rg * 2.0 - 1.0 );
     
-    wsNormal = PerturbNormal( wsNormal, dpdx, dpdy, dhdx, dhdy );
+    vec2 uv = v_txc;
+    uv.y *= -1.0;
+    vec2 duvdx = dFdx( uv ) * bumpness * 64.0;
+    vec2 duvdy = dFdy( uv ) * bumpness * 64.0;
     
-    float ndotl = dot( wsNormal, vec3( 0.0, 1.0, 0.0 ) );
+    float dhdx = ApplyChainRule( bumpGrad.x, bumpGrad.y, duvdx.x, duvdx.y );
+    float dhdy = ApplyChainRule( bumpGrad.x, bumpGrad.y, duvdy.x, duvdy.y );
+
+#endif
+    
+    vec3 wsNormal = PerturbNormal( normalize( v_nrm ), dpdx, dpdy, dhdx, dhdy );
+    
+    float ndotl;
+
+    vec3 diff = vec3( 0.0 );
+    
+    ndotl = dot( wsNormal, normalize( vec3( 1.0, 1.0, 1.0 ) ) );
     ndotl = clamp( ndotl, 0.0, 1.0 );
+    diff += vec3( 0.0, 0.351, 1.0 ) * ndotl;
     
-    gl_FragColor = vec4( vec3( ndotl ), 1.0 );
+    ndotl = dot( wsNormal, normalize( vec3(-1.0,-1.0,-1.0 ) ) );
+    ndotl = clamp( ndotl, 0.0, 1.0 );
+    diff += vec3( 1.0, 0.702, 0.0 ) * ndotl;
+    
+    gl_FragColor = vec4( diff, 1.0 );
 }
