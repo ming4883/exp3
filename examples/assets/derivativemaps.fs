95623688a7912f1d2c390c98835a7937b03ef631
ming <ming4883@gmail.com>
2013-06-23 10:02:36 +0800

    added basic POM support to demo_derivative_maps.html

--- examples/assets/derivativemaps.fs
+++ examples/assets/derivativemaps.fs
@@ -30,7 +30,8 @@ vec3 ParallaxOffset( vec3 v, vec3 n, vec3 dpdx, vec3 dpdy, vec2 duvdx, vec2 duvd
  
     vec2 vscr = vec2( dot( r1, v ), dot( r2, v ) ) / det;
     vec3 vtex;
-    vtex.z  = dot( n, v ) / bumpness;
+    //vtex.z  = dot( n, v ) / parallaxHeight;
+    vtex.z  = dot( n, v );
     vtex.xy = duvdx * vscr.x + duvdy * vscr.y;
     
     return vtex;
@@ -65,6 +66,11 @@ vec3 Lighting( vec3 lightDir, vec3 lightColor, vec3 wsNormal, vec3 wsViewDir )
     return lightColor * ndotl;
 }
 
+float Displacement( float h, float s, float b )
+{
+    return h * s + b;
+}
+
 void main()
 {
     vec3 wsViewDir = normalize( camPos - v_pos );
@@ -76,14 +82,86 @@ void main()
     vec2 duvdx = dFdx( uv );
     vec2 duvdy = dFdy( uv );
     
-    float S = parallaxHeight;
-    float B = S * -0.5;
-    
-    vec3 gradH = texture2D( heightMap, uv ).rgb;
+    vec3 gradH;
     
     if ( useParallax > 0.0 )
     {
-        gradH.z = gradH.z * S + B; 
+        vec3 vViewTS = ParallaxOffset( wsViewDir, normalize( v_nrm ), dpdx, dpdy, duvdx, duvdy );
+        
+        // Compute initial parallax displacement direction:
+        vec2 vParallaxDirection = normalize(  vViewTS.xy );
+           
+        // The length of this vector determines the furthest amount of displacement:
+        float fLength         = length( vViewTS );
+        float fParallaxLength = sqrt( fLength * fLength - vViewTS.z * vViewTS.z ) / vViewTS.z; 
+           
+        // Compute the actual reverse parallax displacement vector:
+        vec2 vParallaxOffsetTS = vParallaxDirection * fParallaxLength;
+           
+        // Need to scale the amount of displacement to account for different height ranges
+        // in height maps. This is controlled by an artist-editable parameter:
+        vParallaxOffsetTS *= parallaxHeight;
+        
+        int nNumSteps   = 64;
+        float fStepSize = 1.0 / float(nNumSteps);
+        float fCurrHeight = 0.0;
+        float fPrevHeight = 1.0;
+        
+        vec2   vTexOffsetPerStep = fStepSize * vParallaxOffsetTS;
+        vec2   vTexCurrentOffset = uv;
+        float  fCurrentBound     = 1.0;
+        float  fParallaxAmount   = 0.0;
+        
+        vec2 pt1;
+        vec2 pt2;
+        
+        for ( int it = 0; it < nNumSteps; ++it )
+        {
+            vTexCurrentOffset -= vTexOffsetPerStep;
+            
+            gradH = texture2D( heightMap, vTexCurrentOffset ).rgb;
+            fCurrHeight = gradH.b;
+            
+            fCurrentBound -= fStepSize;
+            
+            if ( fCurrHeight > fCurrentBound ) 
+            {
+                pt1 = vec2( fCurrentBound, fCurrHeight );
+                pt2 = vec2( fCurrentBound + fStepSize, fPrevHeight );
+
+                break;
+            }
+            
+            fPrevHeight = fCurrHeight;
+        }
+        
+        float fDelta2 = pt2.x - pt2.y;
+        float fDelta1 = pt1.x - pt1.y;
+
+        float fDenominator = fDelta2 - fDelta1;
+
+        // SM 3.0 requires a check for divide by zero, since that operation will generate
+        // an 'Inf' number instead of 0, as previous models (conveniently) did:
+        if ( fDenominator == 0.0 )
+        {
+            fParallaxAmount = 0.0;
+        }
+        else
+        {
+            fParallaxAmount = (pt1.x * fDelta2 - pt2.x * fDelta1 ) / fDenominator;
+        }
+        
+        vec2 vParallaxOffset = vParallaxOffsetTS * ( 1.0 - fParallaxAmount );
+
+        // The computed texture offset for the displaced point on the pseudo-extruded surface:
+        uv = uv - vParallaxOffset;
+        
+        /*
+        float S = parallaxHeight;
+        float B = S * -0.5;
+        
+        gradH = texture2D( heightMap, uv ).rgb;
+        gradH.z = Displacement( gradH.z, S, B );
         
         for ( int it = 0; it < 1; ++it )
         {
@@ -91,9 +169,12 @@ void main()
             uv += vtex.xy * gradH.z;
             
             gradH = texture2D( heightMap, uv ).rgb;
-            gradH.z = gradH.z * S + B; 
+            gradH.z = Displacement( gradH.z, S, B ); 
         }
+        */
     }
+    
+    gradH = texture2D( heightMap, uv ).rgb;
     
     gradH.xy = ( gradH.xy * 2.0 - 1.0 ) * bumpness;
     
